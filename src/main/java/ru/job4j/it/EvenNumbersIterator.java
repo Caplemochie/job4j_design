b0ec061ae9967a490c8eb626e8cacdd9863fac09
Caplemochie <tagirza@yandex.ru>
2021-03-05 04:33:42 +0300

    5.1.2 Added EvenNumbersIterator and EvenIteratorTest

---
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -0,0 +1,39 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return exist() == 1;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        } else {
+            return data[point++];
+        }
+    }
+
+    private Integer exist() {
+        int num = 0;
+        for (int i = point; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                point = i;
+                num++;
+                break;
+            }
+        }
+        return num;
+    }
+}
