847fa000a30c79dfc49ea46999d3e325bffa7ec9
Caplemochie <tagirza@yandex.ru>
2021-03-19 07:38:24 +0300

    5.2.1. Реализовать SimpleArray<T>

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,65 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] array;
+    private int size;
+
+    public SimpleArray(int size) {
+        this.array = new Object[size];
+    }
+
+    public void add(T model) {
+        array[size++] = model;
+    }
+
+    public void set(int index, T model) {
+        Objects.checkIndex(index, size);
+        array[index] = model;
+    }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, size);
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[size] = null;
+        size--;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) array[index];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                return point != size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[point++];
+            }
+        };
+
+    }
+
+    public static void main(String[] args) {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.remove(0);
+        System.out.println(simpleArray.get(0));
+
+    }
+}
---
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,145 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 22.03.2019
+ */
+public class SimpleArrayTest {
+
+    @Test
+    public void whenAddTwoElementsShouldGetTwoElements() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        String[] expected = new String[]{"aa", "bb"};
+        String[] actual = new String[2];
+        actual[0] = simpleArray.get(0);
+        actual[1] = simpleArray.get(1);
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddFreeElementsInArrayTwoShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        simpleArray.add("cc");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddElementsInEmptyArrayShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(0);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        simpleArray.add("cc");
+    }
+
+    @Test
+    public void whenSetByIndexShouldGetSameElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        String expected = "aa";
+        simpleArray.add(expected);
+        simpleArray.set(0, expected);
+        String actual = simpleArray.get(0);
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenSetWithoutAddShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        String expected = "aa";
+        simpleArray.set(0, expected);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenSetByIndexGreaterSizeShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.set(5, "aa");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetByIndexGreaterThenSizeShouldNull() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        simpleArray.get(4);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveWithoutAddShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.remove(0);
+        simpleArray.get(0);
+    }
+
+    @Test
+    public void whenAddAndRemoveOneShouldGetNull() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(3);
+        simpleArray.add("aa");
+        simpleArray.remove(0);
+        assertNull(simpleArray.get(0));
+    }
+
+    @Test
+    public void whenAdd3AndRemoveOneInMiddleAddOneShouldLastAddedHasIndexThree() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        simpleArray.add("cc");
+        simpleArray.remove(1);
+        String expected = "dd";
+        simpleArray.add(expected);
+        assertThat(simpleArray.get(2), is(expected));
+    }
+
+    @Test
+    public void whenAddTwoElementsShouldGetTwoElementsUsingIterator() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        String[] expected = new String[]{"aa", "bb"};
+        String[] actual = new String[2];
+        Iterator<String> iterator = simpleArray.iterator();
+        actual[0] = iterator.next();
+        actual[1] = iterator.next();
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenAddTwoElementsShouldExceptionThenNext3TimesUsingIterator() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        Iterator<String> iterator = simpleArray.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyIteratorShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        Iterator<String> iterator = simpleArray.iterator();
+        iterator.next();
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenFullAddShouldException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("aa");
+        simpleArray.add("bb");
+        simpleArray.add("cc");
+    }
+
+
+}
