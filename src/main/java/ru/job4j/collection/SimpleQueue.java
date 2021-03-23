e999b3e04ef58484dba6070857f3ccf9c87508c7
Caplemochie <tagirza@yandex.ru>
2021-03-23 10:49:23 +0300

    5. Очередь на двух стеках

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (in.size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        if (out.size() == 0) {
+            while (in.size() != 0) {
+                out.push(in.pop());
+            }
+        }
+
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -4,13 +4,20 @@ import ru.job4j.collection.list.ForwardLinked;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
 
     }
 }
