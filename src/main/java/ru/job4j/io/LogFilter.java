a8d17413ea2bc6562beffbb975f5eb86b60f3662
Caplemochie <tagirza@yandex.ru>
2021-04-03 19:28:04 +0300

    0.3. BufferedReader

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -10,7 +10,7 @@ public class LogFilter {
     public static List<String> filter(String file) {
         List<String> result = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            result = in.lines().filter(s -> s.contains("404")).map(s -> s + System.lineSeparator()).collect(Collectors.toList());
+            result = in.lines().filter(s -> s.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -19,6 +19,8 @@ public class LogFilter {
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        for (String str : log) {
+            System.out.println(str);
+        }
     }
 }
