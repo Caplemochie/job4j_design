d08d662edaf7da55713a2555d174ed8c23475b53
Caplemochie <tagirza@yandex.ru>
2021-04-08 02:48:33 +0300

    0.1. FileOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -4,21 +4,16 @@ import java.io.FileOutputStream;
 
 public class ResultFile {
     public static void main(String[] args) {
-        ResultFile resultFile = new ResultFile();
         try (FileOutputStream out = new FileOutputStream("result1.txt")) {
-            out.write(resultFile.multiple(4).getBytes());
+            for (int i = 0; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    out.write(String.format("%d x %d = %d;", i, j, i * j).getBytes());
+                    out.write(System.lineSeparator().getBytes());
+
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public String multiple(int size) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                sb.append(i + " x " + j + " = " + (i * j) + System.lineSeparator());
-            }
-        }
-        return sb.toString();
     }
 }
