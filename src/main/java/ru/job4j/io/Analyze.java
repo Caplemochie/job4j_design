ab4cb6ee5db8866417da9cd4131d9c54285f81a2
Caplemochie <tagirza@yandex.ru>
2021-04-09 16:33:45 +0300

    Анализ доступности сервера(убрал комментарии из main)

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -28,14 +28,11 @@ public class Analyze {
     }
 
     public static void main(String[] args) {
-        Analyze analyze = new Analyze();
-        analyze.unavailable("server.txt", "target.txt");
-
-//        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-//            out.println("15:01:30;15:02:32");
-//            out.println("15:10:30;23:12:32");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
