62ab747ba0df4eaf61d4c44259ffaf45c8ef0667
Caplemochie <tagirza@yandex.ru>
2021-04-09 07:50:21 +0300

    Читаем файл конфигурации

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            values.putAll(read.lines()
+                    .filter(s -> !s.contains("#"))
+                    .flatMap(String::lines)
+                    .map(s -> s.split("="))
+                    .collect(Collectors.toMap(key -> key[0], value -> value[1])));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenGetLastProp() {
+        Config config = new Config("./app.properties");
+        config.load();
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+    @Test
+    public void whenGetLastProp1() {
+        Config config = new Config("./app.properties");
+        config.load();
+        assertNull(config.value("hibernate.connection.password1"));
+    }
+
+    @Test
+    public void whenTryToGetPropWithComment() {
+        Config config = new Config("./app.properties");
+        config.load();
+        assertNull(config.value("#"));
+    }
+}
