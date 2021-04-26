5500a04d6d450642c3edf111b45f32083affd734
Caplemochie <tagirza@yandex.ru>
2021-04-26 21:41:42 +0300

    1. Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+
+
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,14 +15,21 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String answer = "";
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("Bye")) {
-                            server.close();
+                        if (str.contains("=")) {
+                            if (str.contains("Hello")) {
+                                answer = "Hello";
+                            } else if (str.contains("Exit")) {
+                                server.close();
+                            } else {
+                                answer = str.split("=")[1].split(" ")[0];
+                            }
                         }
-
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                 }
             }
         }
